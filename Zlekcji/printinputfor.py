96d9016ca5b1dbb4802bfd79443d257ffd453b52
KodowanieToWyzwanie <148467446+KodowanieToWyzwanie@users.noreply.github.com>
2023-11-07 10:32:22 +0100

    Create printinputfor.py
    wczytywanie danych od użytkownika
    
    pętla for
    
    instrukcja if

---
+++ Zlekcji/printinputfor.py
@@ -0,0 +1,15 @@
+
+a = int(input("Podaj liczbę "))
+b = int(input("Podaj liczbę "))
+c = int(input("Podaj liczbę "))
+
+if a == b and a ==c: #jeżeli
+  print(f"{a} jest takie samo")
+  print(a, "jest takie samo")
+else:
+  print("nie") #w przeciwnym razie 
+
+
+#pętla for
+for wszystko in range(10):
+  print(wszystko+1, end=" ") #end zakończenie linii
